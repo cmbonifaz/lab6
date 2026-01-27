361eb7067b3a0824a3df82702dc9f3b580f0f46c
cmbonifaz <mateobvc@hotmail.com>
2026-01-27 09:54:28 -0500

    Proyecto base con CI

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send("Integraion continua trabajando!");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor trabajando en el puerto ${port}`);
+});
